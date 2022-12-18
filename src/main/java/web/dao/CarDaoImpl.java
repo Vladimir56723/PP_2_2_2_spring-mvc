4de78b3762594afab1b7aa7d1269f4fe3c076ae6
vsazonov <sazonovvv822@gmail.com>
2022-12-18 23:23:24 +0300

    2.2.2.1

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,39 @@
+package web.dao;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarDaoImpl implements CarDao{
+    private static int COUNT;
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car(++COUNT, "BMW", "Black"));
+        cars.add(new Car(++COUNT, "AUDI", "Red"));
+        cars.add(new Car(++COUNT, "MERCEDES", "Blue"));
+        cars.add(new Car(++COUNT, "TOYOTA", "White"));
+        cars.add(new Car(++COUNT, "LADA", "Grey"));
+    }
+
+
+    @Override
+    public List<Car> index(int count) {
+
+        if (count >= cars.size()) {
+            return cars;
+        } else if(count<5&&count>0){
+            List<Car> cars1 = new ArrayList<>();
+            for (int i = 0; i < cars.size(); i++) {
+                if (i <= count-1) {
+                    cars1.add(cars.get(i));
+                }
+            }
+            return cars1;
+        }else {
+            return cars;
+        }
+    }
+}
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,40 +1,19 @@
 package web.service;
 
 import org.springframework.stereotype.Component;
+import web.dao.CarDao;
+import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class CarServiceImpl implements CarService {
-    private static int COUNT;
-    private List<Car> cars;
 
-    {
-        cars = new ArrayList<>();
-        cars.add(new Car(++COUNT, "BMW", "Black"));
-        cars.add(new Car(++COUNT, "AUDI", "Red"));
-        cars.add(new Car(++COUNT, "MERCEDES", "Blue"));
-        cars.add(new Car(++COUNT, "TOYOTA", "White"));
-        cars.add(new Car(++COUNT, "LADA", "Grey"));
-    }
-
+    CarDao carDao = new CarDaoImpl();
 
     @Override
     public List<Car> index(int count) {
-
-        if (count >= cars.size()) {
-            return cars;
-        } else if(count<5&&count>0){
-            List<Car> cars1 = new ArrayList<>();
-            for (int i = 0; i < cars.size(); i++) {
-                if (i <= count-1) {
-                    cars1.add(cars.get(i));
-                }
-            }
-            return cars1;
-        }else {
-            return cars;
-        }
+        return carDao.index(count);
     }
 }
